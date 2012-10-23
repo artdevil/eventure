3a17629c39b313541887ef39acb74893e17fd9c9
artdvl <artdvl91@gmail.com>
2012-10-23 09:25:19 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventure::Application.config.secret_token = '31372797a65d43e94eb3bfad7300174a7a9346245ec5bbaa1f5180d7dc99b22b2194c2089c7e3bdc15b84f6f13b8856182f2399fa4f37b65244339e13d838e11'
